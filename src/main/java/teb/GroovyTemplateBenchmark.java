db0506495eda5b3244daea0861d49a75b06aa564
jycr <jycr@users.noreply.github.com>
2016-10-14 18:11:29 +0200

    feat: Add Groovy Templates

---
+++ src/main/java/teb/GroovyTemplateBenchmark.java
@@ -0,0 +1,38 @@
+package teb;
+
+import java.io.IOException;
+
+import groovy.text.GStringTemplateEngine;
+import groovy.text.Template;
+import teb.util.ClasspathResourceUtils;
+
+/**
+ * @see "http://docs.groovy-lang.org/latest/html/documentation/template-engines.html"
+ */
+public class GroovyTemplateBenchmark extends BaseBenchmark {
+
+	private Template template;
+
+	@Override
+	public void setup() throws Exception {
+		final GStringTemplateEngine engine = new GStringTemplateEngine();
+		template = engine.createTemplate(
+				ClasspathResourceUtils.getReader(
+						getTemplatePath("groovy.jsp")));
+	}
+
+	@Override
+	public void run() {
+		try {
+			template
+					.make(getParams())
+					.writeTo(getOutput());
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void main(final String[] args) {
+		new GroovyTemplateBenchmark().test();
+	}
+}

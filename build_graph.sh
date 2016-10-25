48965072af45f8d619d7b67a8c9d8b9f5e3049f1
jycr <jycr@users.noreply.github.com>
2016-10-25 10:22:36 +0200

    feat: upload result Graph onto GitHub Wiki Pages

--- build_graph.sh
+++ build_graph.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-function buildGraph(){
-	local filename="$1"
-	local title="$2"
+set -o errexit -o nounset
+# errexit: stop executing if any errors occur, by default bash will just continue past any errors to run the next command
+# nounset: stop executing if an unset variable is encountered, by default bash will use an empty string for the value of such variables.
+
+
+TARGET_DIR=benchmark-launcher/target
+
+function buildGraphWithGnuPlot(){
+  local filename="$1"
+  local title="$2"
+  local format="$3"
+  local renderer="$4"
+  if [ -z "$renderer" ]; then
+    renderer="$format"
+  fi
 gnuplot <<EOF
-	# Labels
+  # Labels
 set title '${title}'
 set ylabel 'Template generation time (ns)'
 set xlabel 'Template Engine'
@@ -17,8 +29,8 @@ set autoscale
 set datafile separator ','
 
 # Output
-set terminal pngcairo enhanced font "Arial,11"
-set output 'target/${filename}.png'
+set terminal $renderer enhanced font "Arial,11"
+set output '$TARGET_DIR/${filename}.$format'
 set grid
 set key off
 set boxwidth 0.9 relative
@@ -32,20 +44,43 @@ set style line 2 lc rgb '#808080' lt 1
 set border 3 back ls 2
 set tics nomirror
 
-plot 'target/${filename}.csv' every ::1 using 0:5:xticlabels(stringcolumn(1)) with boxes ls 1,\
-    'target/${filename}.csv' every ::1 using 0:(\$5):(sprintf("%d",\$5)) with labels
+plot '$TARGET_DIR/${filename}.csv' every ::1 using 0:5:xticlabels(stringcolumn(1)) with boxes ls 1,\
+    '$TARGET_DIR/${filename}.csv' every ::1 using 0:(\$5):(sprintf("%d",\$5)) with labels
 EOF
 
 }
 
+function buildGraph(){
+  buildGraphWithGnuPlot "$1" "$2" png pngcairo
+  buildGraphWithGnuPlot "$1" "$2" svg
+}
+
 function filterData() {
-	local templateName="$1"
-	local resultFile="jmh-result-benchmark-$templateName"
-    grep "$templateName" target/jmh-result.csv | grep -v -e 'Rythm\|JMustache\|Jmte\|Handlebars\|Xslt\|Jasper\|Thymeleaf' | sed -r 's,^".+?\.([^."]+?)Benchmark[^"]*","\1",' > target/${resultFile}.csv
-	buildGraph "$resultFile" "Java Template Engine Performance Comparison (template \"${templateName}\")"
-    grep "$templateName" target/jmh-result.csv | grep 'JavaNative\|Rocker\|Rythm\|JMustache\|Jmte\|Handlebars\|Xslt\|Jasper\|Thymeleaf' | sed -r 's,^".+?\.([^."]+?)Benchmark[^"]*","\1",' > target/${resultFile}-others.csv
-	buildGraph "$resultFile-others" "Java Template Engine Performance Comparison (template \"${templateName}\") ... other engines"
+  local templateName="$1"
+  local resultFile="jmh-result-benchmark-$templateName"
+  local otherEngines='Rythm\|JMustache\|Jmte\|Handlebars\|Xslt\|Jasper\|Thymeleaf\|Jamon\|Trimou'
+  
+  head -1 $TARGET_DIR/jmh-result.csv > $TARGET_DIR/${resultFile}.csv
+  grep "$templateName" $TARGET_DIR/jmh-result.csv | grep 'JavaNative' | sed -r 's,^".+?\.([^."]+?)Benchmark[^"]*","\1",' >> $TARGET_DIR/${resultFile}.csv
+  grep "$templateName" $TARGET_DIR/jmh-result.csv | grep -v -e 'JavaNative\|'${otherEngines} | sed -r 's,^".+?\.([^."]+?)Benchmark[^"]*","\1",' >> $TARGET_DIR/${resultFile}.csv
+  buildGraph "$resultFile" "Java Template Engine Performance Comparison (template \"${templateName}\")"
+  
+  head -1 $TARGET_DIR/jmh-result.csv > $TARGET_DIR/${resultFile}-others.csv
+  grep "$templateName" $TARGET_DIR/jmh-result.csv | grep 'JavaNative' | sed -r 's,^".+?\.([^."]+?)Benchmark[^"]*","\1",' >> $TARGET_DIR/${resultFile}-others.csv
+  grep "$templateName" $TARGET_DIR/jmh-result.csv | grep ${otherEngines} | sed -r 's,^".+?\.([^."]+?)Benchmark[^"]*","\1",' >> $TARGET_DIR/${resultFile}-others.csv
+  buildGraph "$resultFile-others" "Java Template Engine Performance Comparison (template \"${templateName}\") ... other engines"
 }
 
 filterData "stocks.html"
 filterData "response.xml"
+
+pushd "$TARGET_DIR"
+	git config --global user.name "Travis"
+	git config --global user.email "travis@travis-ci.org"
+	git clone --quiet https://$GH_TOKEN@github.com/jycr/template-engine-benchmarks.wiki.git
+	pushd "template-engine-benchmarks.wiki"
+		mv ../jmh*.svg ../jmh*.png graph/
+		git add graph/*
+		git push origin
+	popd
+popd
